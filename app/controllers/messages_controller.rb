c01a8ebd4a977ad3da0b57159561a746aafbfde4
kyohei.hayakawa <hayakyo@mac.com>
2018-04-11 11:47:11 +0000

    add column-revision

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   
   def index
-      @messages = Message.all(params[:page])
+      @messages = Message.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
     validates :content, presence: true, length: { maximum: 255 }
     validates :title, presence: true, length: { maximum: 10 }
-     validates :period, : presence: true, length: { maximum: 255 }
+    validates :period, presence: true, length: { maximum: 255 }
 end
