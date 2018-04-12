690056eb22e718748fdd34eaebdfed3c013e4932
kyohei.hayakawa <hayakyo@mac.com>
2018-04-12 09:59:14 +0000

    kadai-bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
 (1..100).each do |number|
-#   Message.create(period: '30171221 ' + number.to_s, title: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
-  Message.create(title: 'test title', content: 'test content ' + number.to_s)
+  Task.create(status: 'test status ' + number.to_s, content: 'test content ' + number.to_s)
 end
